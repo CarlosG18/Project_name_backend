f4f6e37af6bf4fe050ed79ac6fa045336878de56
CarlosG18 <c.gabriel.abc18@gmail.com>
2024-03-29 23:22:54 -0300

    :sparkles: feat: create model curso

--- manicursos/manicursos/urls.py
+++ manicursos/manicursos/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('curso/', include('apps.curso.urls')),
     path('blog/', include('apps.blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
